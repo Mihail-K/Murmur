fd5889fdb0e3d043812f5929373f1beae9a85cd9
Mihail-K <Mihail.K@fimu.tk>
2014-12-18 20:42:26 -0500

    Created a new set of types for component members.

---
+++ src/io/cloudchaser/murmur/types/MurmurField.java
@@ -0,0 +1,52 @@
+/*
+ *	The MIT License (MIT)
+ *
+ *	Copyright (c) 2014 Mihail K
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+
+package io.cloudchaser.murmur.types;
+
+import io.cloudchaser.murmur.symbol.LetSymbol;
+
+/**
+ *
+ * @author Mihail K
+ * @since 0.1
+ **/
+public class MurmurField extends LetSymbol
+		implements MurmurMember {
+	
+	/**
+	 * This method's declaring type.
+	 **/
+	private final MurmurComponent parent;
+
+	public MurmurField(String name, MurmurObject value, MurmurComponent parent) {
+		super(name, value);
+		this.parent = parent;
+	}
+
+	@Override
+	public MurmurComponent getParent() {
+		return parent;
+	}
+	
+}
